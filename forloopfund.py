b4ae11b68aa2b162b50ce7ee69c4ce9f4b693910
mike <jmike1211@gmail.com>
2020-05-07 17:03:33 +0800

    new while loop to funding

---
+++ forloopfund.py
@@ -0,0 +1,79 @@
+import time
+import os
+import sys
+import asyncio
+import functools
+
+from bfxapi import Client
+from bfxmongo import useMongo
+
+sys.path.append('bfxapi')
+from models import FundingCreditModel
+
+API_KEY=""
+API_SECRET=""
+bfx = Client(
+    API_KEY=API_KEY,
+    API_SECRET=API_SECRET,
+    logLevel='INFO'
+)
+
+async def create_funding(amount, frrRate, days=2):
+    print("create funding")
+    response = await bfx.rest.submit_funding_offer("fUSD", amount, frrRate, days)
+    # response is in the form of a Notification object
+    # notify_info is in the form of a FundingOffer
+    print ("Offer: ", response)
+
+async def cancel_funding(offerid):
+    response = await bfx.rest.submit_cancel_funding_offer(offerid)
+    # response is in the form of a Notification object
+    # notify_info is in the form of a FundingOffer
+    print ("Offer: ", response.notify_info)
+
+
+async def cancel_funding_all():
+    response = await bfx.rest.submit_cancel_funding_offer_all("USD")
+    # response is in the form of a Notification object
+    # notify_info is in the form of a FundingOffer
+    print ("Offer: ", response.notify_info)
+
+async def log_wallets():
+    wallets = await bfx.rest.get_wallets()
+    for w in wallets:
+        if w[0] == "funding" and w[1] == "USD":
+            return w[2]i#only catch USD balance
+        else:
+            return 0
+
+async def log_funding_credits():
+    credits = await bfx.rest.get_funding_credits('fUSD')
+    print ("Funding credits:")
+    result = 0
+    for c in credits:
+      result =result + c[FundingCreditModel.AMOUNT] 
+      print(c[FundingCreditModel.AMOUNT])
+    return result
+
+async def run():
+    
+    await cancel_funding_all()
+    balance =await log_wallets()
+    useamount = await log_funding_credits()
+
+    frrRate = useMongo().mongofindone({},"frrrate")["frr"]
+    amount = balance - useamount
+    if amount < 50:
+        print("balance not enough")
+        return
+    if frrRate > 0.00055:
+        days = 30
+        await create_funding(amount, frrRate, days)
+    else:
+        await create_funding(amount, frrRate)
+   
+while True:
+    t = asyncio.ensure_future(run())
+    asyncio.get_event_loop().run_until_complete(t)
+    print("start funding")
+    time.sleep(1200)	
