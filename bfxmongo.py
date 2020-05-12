89ba0505634638c1f11f9af4193b997c126bb963
mike <jmike1211@gmail.com>
2020-05-12 17:34:33 +0800

    add config to save token

--- bfxmongo.py
+++ bfxmongo.py
@@ -2,10 +2,12 @@ import pymongo
 import os
 import sys
 
+from config import Config
 
 class useMongo():
     def __init__(self):
-        self.client = pymongo.MongoClient("")        
+        #self.client = pymongo.MongoClient("mongodb+srv://bfxtest:bfxtest@bitfinextest-blkxg.mongodb.net/test?retryWrites=true&w=majority")        
+        self.client = pymongo.MongoClient(Config.config()["mongoConnect"])        
         self.db = self.client['bfxtest']
 
     def mongofindone(self, token={}, collectionName="account"):
@@ -36,3 +38,49 @@ class useMongo():
         result = collection.update_one(origintoken, {'$set': changetoken})
         return result
 
+    def mongoupsertone(self, origintoken, changetoken, collectionName="account"):
+        collection = self.db[collectionName]
+        modify = collection.find_one(origintoken)
+        print(modify)
+        #modify[changename] = changetoken
+        result = collection.update_one(origintoken, {'$set': changetoken},upsert=True)
+        return result
+
+#result = useMongo().mongodelone({"id" : "20170101"})
+#print(result)
+"""
+mongoResult = {}
+mongoResult["lprice"] = round(0.00002,5)
+
+result = useMongo().mongoupsertone({},mongoResult,"testte")
+print(result)
+print(result.matched_count, result.modified_count)
+
+param = {
+    'frr': '0.00041',
+}
+
+useMongo().mongoinsertone(param, "frrrate")
+
+result = useMongo().mongofindone({},"frrrate")
+print(result)
+if result == None:
+    print("aaaaa")
+else:
+    print(result["frr"])
+
+
+results = useMongo().mongofind({"age" :{'$gt': 20}})
+print(results)
+for i in results:
+    print(i)
+
+mongoResult = {}
+mongoResult["frr"] = round(0.000325613,5)
+mongoResult["hask"] = round(0.000842145613,5)
+mongoResult["lprice"] = round(0.00017313,5)
+
+result = useMongo().mongoupdateone({},mongoResult,"frrrate")
+print(result)
+print(result.matched_count, result.modified_count)
+"""
