a36a34107a8e73ac6d2e6fb6d864b316b81050c0
mike <jmike1211@gmail.com>
2020-06-05 15:39:36 +0800

    modify cal

--- calfundingrate.py
+++ calfundingrate.py
@@ -8,16 +8,18 @@ class calRate:
             frrRate = fundingRate["frr"]
             days = 2 
             if fundingRate["frr"] > 0.00055: # 20%
-                if dayhigh["dayhigh"] > 0.001: #40%
+                if fundingRate["dayhigh"] >= 0.001: #40%
                     frrRate = (fundingRate["frr"] + fundingRate["dayhigh"])/2
-                days = 30
+                elif fundingRate["frr"] >= fundingRate["dayhigh"]: #40%
+                    frrRate = (fundingRate["lprice"] + fundingRate["dayhigh"])/2
+                #days = 30
             else:
-                if fundingRate["ask"] < 0.00019: #7%
+                if fundingRate["lprice"] < 0.00019: #7%
                     frrRate = 0.00025
-                elif fundingRate["ask"] > 0.0004: #15%
-                    frrRate = (fundingRate["frr"] + fundingRate["ask"])/2
+                elif fundingRate["lprice"] > 0.0004: #15%
+                    frrRate = (fundingRate["frr"] + fundingRate["lprice"])/2
         except:
-            frrRate = 0.00027 #10%
+            frrRate = 0.00031123 #10%
             days = 2
         return frrRate, days
-#calRate.fundingRate()
+#a, b = calRate.fundingRate()
